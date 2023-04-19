1c08bab3c2dae5a98ae50f4902fdd5a6429537d5
Md Asif Raza <itsmdasifraza@gmail.com>
2023-04-19 19:40:26 +0530

    Created customized mui tab component
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Home from './components/pages/Home/Home';
+import Dashboard from './components/pages/Dashboard/Dashboard';
 function App() {
   return (
     <div>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
         </Routes>
     </div>
   );
---
+++ src/components/pages/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../common/Header/Header';
+import Tabs from '../../dashboard/Tabs/Tabs';
+const Dashboard = () => {
+  return (
+    <>
+    <Header/>
+    <Tabs/>
+    </>
+  )
+}
+
+export default Dashboard;
